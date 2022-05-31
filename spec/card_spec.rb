6d77e42feb8d410a5d6b9c8e4de3cb160a4b8b45
P Maryon <maryonpawel@gmail.com>
2022-05-31 10:27:24 +0100

    hook implemented to refactor test

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -7,12 +7,15 @@ class Card
 end
 
 RSpec.describe Card do
+  before do
+    puts "I will run before each example"
+    @card = Card.new('Ace','Spades')
+    
+  end
   it "has a rank" do
-    card = Card.new('Ace','Spades')
-    expect(card.rank).to eq('Spades')
+    expect(@card.rank).to eq('Spades')
   end
   it "has a suit" do
-    card = Card.new('Ace','Spades')
-    expect(card.suit).to eq('Ace')
+    expect(@card.suit).to eq('Ace')
   end
 end
